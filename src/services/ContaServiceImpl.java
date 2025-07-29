96652951f51a8d9ee72750182ba26f2385fd2ac4
Josefum1 <juninho.jads@gmail.com>
2025-07-29 20:52:12 -0300

    feat: implementação da classe abstrata ContaServiceImpl, implementação da classe Application refactor: criação do pacote services, movido as classes ContaService e ContaServiceImpl

---
+++ src/services/ContaServiceImpl.java
@@ -0,0 +1,10 @@
+package src.services;
+
+public abstract class ContaServiceImpl implements ContaService {
+
+    private int numeroConta;
+    private String nomeTitular;
+    private String CPF;
+    private double saldo;
+
+}
