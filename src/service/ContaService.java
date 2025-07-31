1232e5f42e24833a4bf2b81df0c07d756267fe69
Josefum1 <juninho.jads@gmail.com>
2025-07-31 20:16:25 -0300

    feat: implementação do método sacar, implementação do método depositar

--- src/service/ContaService.java
+++ src/service/ContaService.java
@@ -2,9 +2,9 @@ package service;
 
 public interface ContaService {
 
-    double sacar(double saldo);
+    double sacar(double saldo, ContaServiceImpl conta);
 
-    double depositar(double saldo);
+    double depositar(double saldo, ContaServiceImpl conta);
 
     double transferir(double saldo, ContaServiceImpl conta);
 }
